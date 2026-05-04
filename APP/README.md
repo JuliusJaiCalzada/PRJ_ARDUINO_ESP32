0be210ed021efc2193d0621aa3bb6e3be9b2ba0e
Julius Jai Calzada <julius.jai@gmail.com>
2026-05-04 21:17:44 +0200

    PAE-3 [PAE][Implementation] To create initial folder structure to organize the different Architectural software component
    PAE-3 [PAE][Implementation] To create initial folder structure to organize the different Architectural software component

--- APP/README.md
+++ APP/README.md
@@ -1,2 +1,4 @@
 # PRJ_ARDUINO_ESP32
 APP folder contains Application of ESP32
+
+The main sketch file is PRJ_ARDUINO_ESP32.ino
---
+++ PRJ_ARDUINO_ESP32.ino
@@ -0,0 +1,19 @@
+// PRJ_ARDUINO_ESP32
+// Basic ESP32 Blink Sketch
+
+#define LED_PIN 2  // ESP32 built-in LED pin
+
+void setup() {
+  // Initialize the LED pin as an output
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  // Turn the LED on
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);  // Wait for 1 second
+
+  // Turn the LED off
+  digitalWrite(LED_PIN, LOW);
+  delay(1000);  // Wait for 1 second
+}
