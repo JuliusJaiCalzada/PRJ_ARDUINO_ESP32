3af96e8be8d2f0ac4b0d960350c5222dca6dfb90
Julius Jai Calzada <julius.jai@gmail.com>
2026-05-04 21:55:39 +0200

    Add template files for APP, DATA, and LIB folders

---
+++ LIB/LibTemplate.c
@@ -0,0 +1,24 @@
+/*
+Project:
+Author:
+Date:
+Description: Library Template Implementation
+*/
+
+#include "LibTemplate.h"
+
+void LibInit(void)
+{
+    // Initialize library-specific components here
+}
+
+void LibFunction1(void)
+{
+    // Implement library function 1
+}
+
+void LibFunction2(int param)
+{
+    // Implement library function 2
+    // param: input parameter
+}
---
+++ LIB/LibTemplate.h
@@ -0,0 +1,16 @@
+/*
+Project:
+Author:
+Date:
+Description: Library Template Header
+*/
+
+#ifndef LIBTEMPLATE_H
+#define LIBTEMPLATE_H
+
+// Function prototypes
+void LibInit(void);
+void LibFunction1(void);
+void LibFunction2(int param);
+
+#endif // LIBTEMPLATE_H
